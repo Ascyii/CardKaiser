4094c6aeedad3f0c9da68fc568c974267ea53ebd
Ascyii <jonashahn1@gmx.net>
2021-12-16 22:03:40 -0800

    Finished core function of game!

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CardKaiser:
-####To start the game: python runkaiser.py
-####Read the rules in the rules.txt
-######Classic Kaiser game
-######Later I will at AI function
+##To start the game: python runkaiser.py
+###Read the rules in the rules.txt
+###Game structure is found in gamestructure.txt
+###Classic Kaiser game
+###Later I will at AI function
--- runkaiser.py
+++ runkaiser.py
@@ -3,6 +3,11 @@ import json
 import random
 import sys
 
+# TODO: output cleanup; structure cleanup; implement rules
+# TODO: better won trick funktion; better is playable function
+# TODO: implement rule greater 47 less 46
+
+
 CARD_FILE = "cardproperties.json"
 with open(CARD_FILE, "r") as file:
     CARDPROPERTIES = json.load(file)
@@ -154,27 +159,69 @@ class Table:
         else:
             return False
 
-    def get_winner_of_trick(self, trump) -> Team:
+    def get_winner_of_trick(self, trump):
+        # when no then player with the highest card in leading suit wins
+        # when trump != N then player with the highest card in leading suit when no trump is played
+        # otherwise player with the highest trump card wins
+        # function returns team not player
         leading_suit = self.current_cards[0][0].get_suit()
-        winner = self.current_cards[0][1].team
         if trump == "N":
-            winner = self.current_cards[0][1].team
-            print(leading_suit)
+            potential_cards = []
             for card_container in self.current_cards:
                 card = card_container[0]
-                player_of_card = card_container[1]
-        return winner
+                if card.suit == leading_suit:
+                    potential_cards.append(card_container)
+            return max(potential_cards)[1]
+        else:
+            potential_cards = []
+            potential_trumps = []
+            for card_container in self.current_cards:
+                card = card_container[0]
+                if leading_suit == trump == card.suit:
+                    potential_trumps.append(card_container)
+                    continue
+                elif leading_suit == card.suit:
+                    potential_cards.append(card_container)
+                elif trump == card.suit:
+                    potential_trumps.append(card_container)
+            if not potential_trumps:
+                return max(potential_cards)[1]
+            return max(potential_trumps)[1]
+
+    def get_cards_with_players(self):
+        string = "Played cards "
+        for card_container in self.current_cards:
+            card = card_container[0]
+            player_name = card_container[1].name
+            string += " : " + str(card)
+            string += " by " + str(player_name)
+        return string
 
     def pack_to_trick(self, deck) -> Trick:
-        trick = Trick(self.current_cards)
+        actual_cards = []
+        for card_container in self.current_cards:
+            actual_cards.append(card_container[0])
+        trick = Trick(actual_cards)
         self.clear(deck)
         return trick
 
-    def play(self, player, card):
+    def play(self, card, player):
         if not self.check_full():
             self.current_cards.append([card, player])
         else:
             raise ValueError("cant play more than 4 cards at once")
+
+    def check_playable(self, card, player):
+        if len(self.current_cards) <= 0:
+            return True
+        leading_suit = self.current_cards[0][0].suit
+        if card.suit == leading_suit:
+            return True
+        elif not player.has_suit(leading_suit):
+            return True
+        elif player.has_suit(leading_suit):
+            print("you have to follow suit")
+            return False
 
 
 class Player:
@@ -196,6 +243,7 @@ class Player:
 
     def play(self, card, table):
         table.play(card, self)
+        self.hand.remove(card)
 
     def draw(self, deck: Deck, amount: int = 1):
         for _ in range(amount):
@@ -216,6 +264,9 @@ class Player:
 
     def sortHand(self):
         self.hand.sort()
+
+    def has_suit(self, suit):
+        return any(card.suit == suit for card in self.hand)
 
 
 class Game:
@@ -276,6 +327,8 @@ class Game:
             player.clear(self.deck)
 
     def reset(self):
+        self.highest_bid = 6
+        self.have_no = True
         self.clear_hands()
         self.deck.shuffle()
         self.deal_cards()
@@ -300,6 +353,7 @@ class Game:
             if len(inp) > 2:
                 if inp[2:4] == "no":
                     have_no = True
+                    self.trump = "N"
             if bid_value == 0:
                 print("You passed.")
                 break
@@ -317,10 +371,14 @@ class Game:
             try:
                 inp = int(self.ask_for_input())
                 card = hand[inp - 1]
-                break
             except (IndexError, ValueError):
                 print("enter valid index")
-        return card
+                continue
+            if self.table.check_playable(card, self.current_player):
+                return card
+            else:
+                print("You cant play this card.")
+                continue
 
     def ask_for_trump(self):
         suits = CARDPROPERTIES["suits"]
@@ -331,25 +389,47 @@ class Game:
             else:
                 print("enter suit D, H, S, C:  ")
 
+    def update_team_after_round(self, team):
+        team.determine_trick_score()
+        if team == self.bidding_team:
+            if team.trick_value >= self.highest_bid:
+                if not self.have_no:
+                    team.score += team.trick_value
+                else:
+                    team.score += team.trick_value * 2
+            elif team.trick_value < self.highest_bid:
+                if not self.have_no:
+                    team.score -= self.highest_bid
+                else:
+                    team.score -= self.highest_bid * 2
+        team.clear_tricks()
+
     def round(self):
         for _ in range(len(self.players)):
+            if self.highest_bid == 6 and self.have_no:
+                print("You are starting with bidding")
+            else:
+                print(f"the highest bid is {self.highest_bid} with no {self.have_no}")
             print(f"{self.current_bidder.name} is bidding.")
             self.current_bidder.show()
             self.ask_for_bid()
             self.current_bidder = self.next_player(self.current_bidder)
         if self.highest_bid == 6:
             self.highest_bid = 7
-            self.current_player = self.current_bidder
+            self.current_player = self.current_dealer
             self.bidding_team = self.current_bidder.team
             while True:
-                inp = str(input("Do you want to play no? (y or n) "))
-                if inp == "y":
+                inp = str(input("Do you want to choose suit? (y or n) "))
+                if inp == "n":
                     self.have_no = True
+                    self.trump = "N"
                     break
-                elif inp == "n":
+                elif inp == "y":
                     self.have_no = False
                     break
 
+        print(f"{self.current_player} starts with playing.")
+        print(f"{self.bidding_team} won the bid with {self.highest_bid} and no {self.have_no}.")
         if not self.have_no:
             print("enter trump")
             self.trump = self.ask_for_trump()
@@ -358,40 +438,54 @@ class Game:
             self.trump = "N"
             print("nothing is trump")
 
-        print(f"{self.current_player} starts with playing.")
-        print(f"{self.bidding_team} won the bid with {self.highest_bid} and no {self.have_no}.")
-
-        round_active = True
-        while round_active:
+        # Play all cards
+        for round_count in range(8):
             for _ in range(4):
                 player = self.current_player
                 player.show()
+                print(self.table.get_cards_with_players())
                 while True:
                     hovered_card = self.ask_for_card(player.hand)
-                    if self.check_playable(hovered_card, player):
+                    if self.table.check_playable(hovered_card, player):
                         player.play(hovered_card, self.table)
                         break
                     else:
                         print("You cant play this card.")
                 self.current_player = self.next_player(self.current_player)
 
+            print(self.table.get_cards_with_players())
             trick_winner = self.table.get_winner_of_trick(self.trump)
             trick = self.table.pack_to_trick(self.deck)
-            trick_winner.add_trick(trick)
-            print(f"{trick_winner} won the Trick")
+            trick_winner.team.add_trick(trick)
+            self.current_player = trick_winner
+            print(f"{trick_winner.name} in Team{trick_winner.team.id} won the Trick")
             print(f"{self.current_player} is starting new.")
 
     def calculate_score(self):
+        # Calculate score of tricks
+        # Add trick score or winning score after rules to final score
+        # Print the final scores of team
+        for team in self.teams:
+            self.update_team_after_round(team)
         print(f"The new scores after this round are: {[t for t in self.teams]}")
 
-    def check_playable(self, card, player):
-        pass
+    def check_win_or_loss(self):
+        for team in self.teams:
+            if team.score >= 52:
+                print(f"Team{team.id} won the game!")
+                self.running = False
+            elif team.score <= -52:
+                print(f"Team{team.id} lost the game. The other team won!")
+                self.running = False
 
     def loop(self):
         while self.running:
             self.reset()
             self.round()
             self.calculate_score()
+            self.check_win_or_loss()
+            if self.running:
+                self.current_dealer = self.next_player(self.current_dealer)
 
 
 def main():
